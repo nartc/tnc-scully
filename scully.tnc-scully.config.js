8b985f48147508dafab622bc0ec89de2cf2f3097
Chau Tran <ctch5@mail.umsl.edu>
2020-06-01 19:42:28 -0500

    update scully config

--- scully.tnc-scully.config.js
+++ scully.tnc-scully.config.js
@@ -3,13 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var scully_plugin_http404_1 = require("@gammastream/scully-plugin-http404");
 var scully_1 = require("@scullyio/scully");
 var scully_plugin_minify_html_1 = require("scully-plugin-minify-html");
-require("./plugins/about-post-render");
 scully_1.setPluginConfig('md', { enableSyntaxHighlighting: true });
 exports.config = {
     projectRoot: './src',
     projectName: 'tnc-scully',
     outDir: './dist/static',
-    defaultPostRenderers: [scully_plugin_http404_1.getHttp404Plugin(), 'httpAbout'],
+    defaultPostRenderers: [scully_plugin_http404_1.getHttp404Plugin()],
     routes: {
         '/blog/:slug': {
             type: 'contentFolder',
