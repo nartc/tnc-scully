8ad1cae82d74078c7e79b00b5f6ab6c476804f21
Chau Tran <ctch5@mail.umsl.edu>
2020-06-03 13:55:59 -0500

    temporarily disable service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { ScullyLibModule } from '@scullyio/ng-lib';
-import { environment } from '../environments/environment';
 import { appRoutes } from './app-routes';
 
 import { AppComponent } from './app.component';
@@ -11,12 +9,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes),
-    ScullyLibModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), ScullyLibModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
