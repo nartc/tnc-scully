9bc42a4062dd12031ddcf5dcb54738ef5cb72a04
Chau Tran <ctch5@mail.umsl.edu>
2020-06-03 13:15:24 -0500

    disable angular for prod build

--- scully.tnc-scully.config.js
+++ scully.tnc-scully.config.js
@@ -2,20 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var scully_plugin_http404_1 = require("@gammastream/scully-plugin-http404");
 var scully_1 = require("@scullyio/scully");
+var scully_plugin_disable_angular_1 = require("scully-plugin-disable-angular");
 var scully_plugin_minify_html_1 = require("scully-plugin-minify-html");
 scully_1.setPluginConfig('md', { enableSyntaxHighlighting: true });
+scully_1.setPluginConfig(scully_plugin_disable_angular_1.DisableAngular, 'render', {
+    removeState: true,
+});
 exports.config = {
     projectRoot: './src',
     projectName: 'tnc-scully',
     outDir: './dist/static',
-    defaultPostRenderers: [scully_plugin_http404_1.getHttp404Plugin(), 'seoHrefOptimise'],
+    defaultPostRenderers: [scully_plugin_http404_1.getHttp404Plugin(), 'seoHrefOptimise', scully_plugin_disable_angular_1.DisableAngular],
     routes: {
         '/blog/:slug': {
-            type: 'contentFolder',
+            type: scully_1.RouteTypes.contentFolder,
             slug: {
                 folder: './blog',
             },
-            postRenderers: [scully_plugin_minify_html_1.MinifyHtml],
+            postRenderers: [scully_plugin_minify_html_1.MinifyHtml, scully_plugin_disable_angular_1.DisableAngular],
         },
     },
 };
--- scully.tnc-scully.config.ts
+++ scully.tnc-scully.config.ts
@@ -1,21 +1,25 @@
 import { getHttp404Plugin } from '@gammastream/scully-plugin-http404';
-import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { RouteTypes, ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { DisableAngular } from 'scully-plugin-disable-angular';
 import { MinifyHtml } from 'scully-plugin-minify-html';
 
 setPluginConfig('md', { enableSyntaxHighlighting: true });
+setPluginConfig(DisableAngular, 'render', {
+  removeState: true,
+});
 
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'tnc-scully',
   outDir: './dist/static',
-  defaultPostRenderers: [getHttp404Plugin(), 'seoHrefOptimise'],
+  defaultPostRenderers: [getHttp404Plugin(), 'seoHrefOptimise', DisableAngular],
   routes: {
     '/blog/:slug': {
-      type: 'contentFolder',
+      type: RouteTypes.contentFolder,
       slug: {
         folder: './blog',
       },
-      postRenderers: [MinifyHtml],
+      postRenderers: [MinifyHtml, DisableAngular],
     },
   },
 };
