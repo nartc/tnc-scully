36aa05b8bec3ec05a5903d8842c46b9a341bfc05
Chau Tran <ctch5@mail.umsl.edu>
2020-08-18 12:00:38 -0500

    update packages and adjust avatar component

--- src/app/home/components/avatar/avatar.component.ts
+++ src/app/home/components/avatar/avatar.component.ts
@@ -8,25 +8,24 @@ import { Destroyable } from '../../../shared/destroyable';
   selector: 'app-avatar',
   template: `
     <div
-      class="w-16 h-16 relative mb-4"
-      role="img"
-      aria-label="Avatar of blog author"
-      *ngIf="{ hasUpdate: hasUpdate$ | async } as vm"
-      [ngClass]="{ 'pointer-events-none': !vm.hasUpdate, 'cursor-pointer': vm.hasUpdate }"
-      (click)="reload(vm.hasUpdate)"
+        class="w-16 h-16 relative mb-4"
+        [class.pointer-events-none]="!vm.hasUpdate"
+        [class.cursor-pointer]="vm.hasUpdate"
+        role="img"
+        aria-label="Avatar of blog author"
+        *ngIf="{ hasUpdate: hasUpdate$ | async } as vm"
+        (click)="reload(vm.hasUpdate)"
     >
       <div
-        class="group w-full h-full rounded-full border-4 border-transparent text-center flex items-center relative avatar"
-        [attr.data-tooltip]="'New version available 🚀'"
-        [ngClass]="{ 'show cursor-pointer': vm.hasUpdate }"
+          class="group w-full h-full rounded-full border-4 border-transparent text-center flex items-center relative avatar"
+          [class.show]="vm.hasUpdate"
+          [class.cursor-pointer]="vm.hasUpdate"
+          [attr.data-tooltip]="'New version available 🚀'"
       >
-        <span class="hidden group-hover:table-cell text-white font-bold align-middle">
-          KR
-        </span>
         <img
-          src="https://avatars1.githubusercontent.com/u/25516557?s=460&u=b15a0b6c89d6d8d0b7225a6eab30c060f70d3d9f&v=4"
-          alt="avatar"
-          class="object-cover object-center rounded-full w-full h-full visible group-hover:hidden"
+            src="https://avatars1.githubusercontent.com/u/25516557?s=460&u=b15a0b6c89d6d8d0b7225a6eab30c060f70d3d9f&v=4"
+            alt="avatar"
+            class="object-cover object-center rounded-full w-full h-full visible group-hover:hidden"
         />
       </div>
     </div>
