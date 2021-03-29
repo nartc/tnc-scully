b7d1c468ae65351f1f511ad093db82b071cb1086
Chau Tran <nartc7789@gmail.com>
2021-03-29 10:35:14 -0500

    add swUpdate check in blog component to force update

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import { ScullyRoutesService } from '@scullyio/ng-lib';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { defer, EMPTY, from, Observable, of } from 'rxjs';
+import { mapTo, startWith, switchMap, tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { Frontmatter } from '../shared/frontmatter';
 import { MetaService } from '../shared/services/meta.service';
@@ -15,15 +16,35 @@ import { MetaService } from '../shared/services/meta.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BlogComponent {
-  blog$: Observable<Frontmatter> = this.scullyRoutesService.getCurrent().pipe(
-    tap((blog: Frontmatter) => {
-      this.metaService.update({ ...blog, url: `${environment.baseUrl}${blog.route}` });
+  isProd = environment.production;
+  blog$: Observable<Frontmatter> = of(this.isProd).pipe(
+    switchMap((isProd) => {
+      return defer(() => {
+        if (isProd && this.swUpdate.isEnabled) {
+          return from(this.swUpdate.checkForUpdate()).pipe(
+            switchMap(() => this.swUpdate.available.pipe(mapTo(true), startWith(false))),
+            switchMap((hasUpdate) => {
+              if (hasUpdate) {
+                this.swUpdate.checkForUpdate().then(() => window?.location?.reload());
+                return EMPTY;
+              }
+              return this.scullyRoutesService.getCurrent();
+            }),
+          );
+        }
+
+        return this.scullyRoutesService.getCurrent();
+      }).pipe(
+        tap((blog: Frontmatter) => {
+          this.metaService.update({ ...blog, url: `${environment.baseUrl}${blog.route}` });
+        }),
+      );
     }),
   );
-  isProd = environment.production;
 
   constructor(
     private readonly scullyRoutesService: ScullyRoutesService,
     private readonly metaService: MetaService,
+    private readonly swUpdate: SwUpdate,
   ) {}
 }
