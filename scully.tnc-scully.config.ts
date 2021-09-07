774e3503eb104ad401fd3626e5f23d5d652d4e18
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2021-09-07 07:57:07 -0500

    clean up

--- scully.tnc-scully.config.ts
+++ scully.tnc-scully.config.ts
@@ -19,10 +19,6 @@ export const config: ScullyConfig = {
   defaultPostRenderers: [MinifyHtml, getHttp404Plugin(), 'seoHrefOptimise', 'lazyImages'],
   routes: {
     '/blog/:slug': {
-      // type: RouteTypes.contentFolder,
-      // slug: {
-      //   folder: './blog',
-      // },
       type: NotionDomRouter,
       postRenderers: [NotionDom],
       databaseId: '52b3f140bcfc40d5b101536799b059b4',
